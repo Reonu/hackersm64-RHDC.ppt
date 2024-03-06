921cc200bc4f67e59c400e51b0671e663f087a49
Reonu <danileon95@gmail.com>
2024-03-06 13:13:51 +0000

    Add position data

---
+++ actors/confroom/data.c
@@ -0,0 +1,6 @@
+#include "types.h"
+
+f32 confroom_projScreenPos[3] = {     4.4792354107f,   143.5177445412f,   911.3306045532f };
+
+f32 confroom_cameraPos[3] = {     0.0000000000f,   134.3339681625f,   414.8766040802f };
+
---
+++ actors/confroom/data.h
@@ -0,0 +1,4 @@
+#pragma once
+
+extern f32 confroom_projScreenPos[3];
+extern f32 confroom_cameraPos[3];
