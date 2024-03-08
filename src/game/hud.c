bb9acdc4a7e5ce3a4a009bb792eb4e0509adc3ee
Reonu <danileon95@gmail.com>
2024-03-08 23:54:01 +0000

    do not render power meter

--- src/game/hud.c
+++ src/game/hud.c
@@ -594,7 +594,7 @@ void render_hud(void) {
 #endif
 
         if (hudDisplayFlags & HUD_DISPLAY_FLAG_CAMERA_AND_POWER) {
-            render_hud_power_meter();
+            //render_hud_power_meter();
 #ifdef PUPPYCAM
             if (!gPuppyCam.enabled) {
 #endif
