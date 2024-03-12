5bc0af1f50319e0e0d5af9609b7416e15bd57867
Reonu <danileon95@gmail.com>
2024-03-12 21:05:39 +0000

    slide 11

---
+++ levels/slides/area_11/geo.inc.c
@@ -0,0 +1,30 @@
+#include "src/game/envfx_snow.h"
+
+const GeoLayout slides_area_11_geo[] = {
+	GEO_NODE_START(),
+	GEO_OPEN_NODE(),
+		GEO_ROTATION_NODE_WITH_DL(LAYER_FORCE, 90, 0, 0, slides_dl_0slide10bg_001_mesh_layer_0),
+		GEO_TRANSLATE_NODE_WITH_DL(LAYER_FORCE, 1627, 1060, 514, slides_dl_1mmmMesh_001_mesh_layer_0),
+		GEO_TRANSLATE_NODE_WITH_DL(LAYER_FORCE, -43, -967, 12, slides_dl_manorslidebilinearMesh_mesh_layer_0),
+		GEO_TRANSLATE_NODE_WITH_DL(LAYER_FORCE, -1047, 951, 0, slides_dl_manortitleMesh_mesh_layer_0),
+	GEO_CLOSE_NODE(),
+	GEO_RETURN(),
+};
+const GeoLayout slides_area_11[] = {
+	GEO_NODE_SCREEN_AREA(10, SCREEN_WIDTH/2, SCREEN_HEIGHT/2, SCREEN_WIDTH/2, SCREEN_HEIGHT/2),
+	GEO_OPEN_NODE(),
+		GEO_ZBUFFER(1),
+		GEO_OPEN_NODE(),
+			GEO_CAMERA_FRUSTUM_WITH_FUNC(45.0000, 100, 30000, geo_camera_fov),
+			GEO_OPEN_NODE(),
+				GEO_CAMERA(CAMERA_MODE_8_DIRECTIONS, 1000000, 0, 0, 1000000, -100, 0, geo_camera_main),
+				GEO_OPEN_NODE(),
+					GEO_BRANCH(1, slides_area_11_geo),
+					GEO_RENDER_OBJ(),
+					GEO_ASM(ENVFX_MODE_NONE, geo_envfx_main),
+				GEO_CLOSE_NODE(),
+			GEO_CLOSE_NODE(),
+		GEO_CLOSE_NODE(),
+	GEO_CLOSE_NODE(),
+	GEO_END(),
+};
--- levels/slides/geo.inc.c
+++ levels/slides/geo.inc.c
@@ -1,5 +1,6 @@
 #include "levels/slides/area_1/geo.inc.c"
 #include "levels/slides/area_10/geo.inc.c"
+#include "levels/slides/area_11/geo.inc.c"
 #include "levels/slides/area_2/geo.inc.c"
 #include "levels/slides/area_3/geo.inc.c"
 #include "levels/slides/area_4/geo.inc.c"
