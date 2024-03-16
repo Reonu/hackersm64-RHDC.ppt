63d9c58151f84c0e8ebf06566b57c4c652047720
Reonu <danileon95@gmail.com>
2024-03-16 19:34:25 +0000

    Push boxes

--- actors/confroom/data.h
+++ actors/confroom/data.h
@@ -2,3 +2,9 @@
 
 extern f32 confroom_projScreenPos[3];
 extern f32 confroom_cameraPos[3];
+extern f32 confroom_spaceMainRoom[2][3];
+extern f32 confroom_spaceDoorToHall[2][3];
+extern f32 confroom_colDesk2[2][3];
+extern f32 confroom_colDesk1[2][3];
+extern f32 confroom_colDesk3[2][3];
+extern f32 confroom_colDesk4[2][3];
