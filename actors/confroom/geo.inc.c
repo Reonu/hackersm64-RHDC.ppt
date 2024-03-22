b2538dd24f2db819e40bf293e8fbbbeb7f35a0d5
Reonu <danileon95@gmail.com>
2024-03-22 01:00:48 +0000

    confroom changes

--- actors/confroom/geo.inc.c
+++ actors/confroom/geo.inc.c
@@ -11,6 +11,7 @@ const GeoLayout confroom_geo[] = {
 		GEO_TRANSLATE_NODE_WITH_DL(LAYER_OPAQUE, 696, 194, 1582, confroom_office_mesh_layer_1),
 		GEO_OPEN_NODE(),
 			GEO_DISPLAY_LIST(LAYER_ALPHA, confroom_office_mesh_layer_4),
+			GEO_DISPLAY_LIST(LAYER_TRANSPARENT, confroom_office_mesh_layer_5),
 		GEO_CLOSE_NODE(),
 	GEO_CLOSE_NODE(),
 	GEO_END(),
