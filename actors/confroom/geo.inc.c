86cdde9887c0676c78288f3a3d2138af62ed4fa5
Reonu <danileon95@gmail.com>
2024-03-23 13:53:25 +0000

    uncringe confroom

--- actors/confroom/geo.inc.c
+++ actors/confroom/geo.inc.c
@@ -7,7 +7,6 @@ const GeoLayout confroom_geo[] = {
 		GEO_DISPLAY_LIST(LAYER_OPAQUE, confroom_000exterior_mesh_layer_1),
 		GEO_ASM(LAYER_OPAQUE, geo_render_projector_screen),
 		GEO_TRANSLATE_NODE_WITH_DL(LAYER_OPAQUE, 8, 194, 339, confroom_confroomMesh_mesh_layer_1),
-		GEO_DISPLAY_LIST(LAYER_OPAQUE, confroom_projector_mesh_layer_1),
 		GEO_TRANSLATE_NODE_WITH_DL(LAYER_OPAQUE, 11, 38, 386, confroom_tables_mesh_layer_1),
 		GEO_TRANSLATE_NODE_WITH_DL(LAYER_OPAQUE, 696, 194, 1582, confroom_office_mesh_layer_1),
 		GEO_OPEN_NODE(),
