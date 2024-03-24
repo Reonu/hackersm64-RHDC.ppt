ad496326bfae12cb143f06b4f45645963d6d7d4d
Reonu <danileon95@gmail.com>
2024-03-24 14:47:34 +0000

    arm animations

--- actors/arm/anims/data.inc.c
+++ actors/arm/anims/data.inc.c
@@ -1,2 +1,3 @@
 #include "anim_arm_raise.inc.c"
 #include "anim_arm_idle.inc.c"
+#include "anim_arm_drink.inc.c"
--- actors/arm/anims/table.inc.c
+++ actors/arm/anims/table.inc.c
@@ -1,5 +1,6 @@
 const struct Animation *const arm_anims[] = {
 	&arm_anim_arm_raise,
 	&arm_anim_arm_idle,
+	&arm_anim_arm_drink,
 	NULL,
 };
