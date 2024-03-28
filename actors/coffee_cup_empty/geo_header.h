6b7a2f8e245b99dbbffbe78592facd80f17bcc9e
Reonu <danileon95@gmail.com>
2024-03-28 13:02:02 +0000

    fix cup again

--- actors/coffee_cup_empty/geo_header.h
+++ actors/coffee_cup_empty/geo_header.h
@@ -1,7 +1,6 @@
 extern const GeoLayout coffee_cup_empty_geo[];
-extern Vtx coffee_cup_empty_coffeemptyobject_mesh_layer_1_vtx_cull[8];
-extern Vtx coffee_cup_empty_coffeemptyobject_mesh_layer_1_vtx_0[17];
-extern Gfx coffee_cup_empty_coffeemptyobject_mesh_layer_1_tri_0[];
+extern Vtx coffee_cup_empty_Cylinder_002_mesh_layer_1_vtx_cull[8];
+extern Vtx coffee_cup_empty_Cylinder_002_mesh_layer_1_vtx_0[56];
+extern Gfx coffee_cup_empty_Cylinder_002_mesh_layer_1_tri_0[];
 extern Gfx mat_coffee_cup_empty_cup[];
-extern Gfx mat_revert_coffee_cup_empty_cup[];
-extern Gfx coffee_cup_empty_coffeemptyobject_mesh_layer_1[];
+extern Gfx coffee_cup_empty_Cylinder_002_mesh_layer_1[];
